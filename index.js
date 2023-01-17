1d5fb070287f8f4abb5a0836750807fcd7058fda
Prashantcoder88 <115200889+Prashantcoder88@users.noreply.github.com>
2023-01-17 17:00:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const http =require("http");
 
 const server =http.createServer((req,res)=>{
-    res.end('Hello from the other sides');
+    //console.log(req.url);
+    if(req.url="/"){
+        res.end('Hello from the Home sides');
+    }
+   else if(req.res="/about")
+   {
+        res.end('Hello from the Aboutus side');
+   }
+   else if(req.res="/contact")
+   {
+        res.end('Hello from the Contact side');
+   }
+   else{
+    res.end("404 error page")
+   }
 });
 
-server.listen(8000,"127.0.0.1",()=>{
-    console.log('listening to the port no. 8000');
+server.listen(3000,"127.0.0.1",()=>{
+    console.log('listening to the port no. 3000');
 });
